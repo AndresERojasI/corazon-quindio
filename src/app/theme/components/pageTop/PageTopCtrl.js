1b5766e4536e82348cede443c0c58e9cf817db66
AndresRojas <andres.rojas.isaza@gmail.com>
2016-06-28 16:12:20 +0000

    Top menu fixes on CSS

--- src/app/theme/components/pageTop/PageTopCtrl.js
+++ src/app/theme/components/pageTop/PageTopCtrl.js
@@ -9,8 +9,25 @@
       .controller('PageTopCtrl', PageTopCtrl);
 
   /** @ngInject */
-  function PageTopCtrl($scope, sidebarService) {
-  	$scope.menu = sidebarService.getMenuItems();
-  	console.log($scope.menu);
+  function PageTopCtrl($scope, sidebarService, $location) {
+  	drawMenu();
+  	$scope.$on('$locationChangeSuccess', function() {
+        drawMenu();
+    });
+
+    function drawMenu(){
+    	var menuItems = sidebarService.getMenuItems();
+
+	  	for (var i = 0, len = menuItems.length; i < len; i++) {
+	  		var rootName = menuItems[i].root.replace('#', '');
+	  		
+	  		if($location.$$url == rootName){
+	  			menuItems[i].active = true;
+	  			break;
+	  		}
+	  	}
+
+	  	$scope.menu = menuItems;
+    }
   }
 })();
